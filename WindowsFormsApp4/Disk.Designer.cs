9e7cdbccc8743c7636dc2b4e1486888b3dcef734
俊林 邓 <906163215@qq.com>
2021-11-11 18:57:49 +0800

    添加项目文件。

---
+++ WindowsFormsApp4/Disk.Designer.cs
@@ -0,0 +1,356 @@
+﻿
+namespace DiskTest11
+{
+    partial class Disk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 712);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 37);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "开始测试";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(767, 694);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Disk selection";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox3);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Location = new System.Drawing.Point(34, 212);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(673, 459);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Edit details";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "随机",
+            "顺序",
+            "循环"});
+            this.comboBox1.Location = new System.Drawing.Point(153, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(488, 39);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(153, 360);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 39);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(153, 310);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 39);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(153, 184);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 39);
+            this.textBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.comboBox2.DisplayMember = "1";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.comboBox2.Location = new System.Drawing.Point(153, 248);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(130, 39);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 363);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 31);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "测试时间";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 31);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "测试次数";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Block size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "测试容量";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "测试模式";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox2.Location = new System.Drawing.Point(6, 27);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(200, 35);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "Test this drive";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(34, 28);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(686, 35);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Automatically select all fixed drives at the start of testing";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(34, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(673, 137);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "扇区大小";
+            this.Column5.MinimumWidth = 8;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "扇区总数";
+            this.Column4.MinimumWidth = 8;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "属性";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Size";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Drive";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(153, 406);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(130, 39);
+            this.textBox5.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 409);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 31);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "循环次数";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "随机",
+            "顺序",
+            "循环"});
+            this.comboBox3.Location = new System.Drawing.Point(153, 129);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(130, 39);
+            this.comboBox3.TabIndex = 21;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 31);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "数据类型";
+            // 
+            // Disk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 761);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Disk";
+            this.Text = "Disk";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiskTestLib;
+
+namespace DiskTest11
+{
+
+    public partial class Form1 :Sunny.UI.UIForm
+    {
+        private const int DEAFAUT_BLOCKSIZE = 512;
+        ArrayList Disk_Driver_List = new ArrayList();
+        ArrayList Disk_Informaion_List = new ArrayList();
+        private byte[] TestArray;
+        private byte[] CompareArray;
+        public Form1()
+        {
+            InitializeComponent();
+            Init_Disk_Information();
+            Init_Disk_Driver();
+            OrderWriteAndVerify(0, 100, 1, 1, 1);
+            int pageIndex = 100;
+            TreeNode parent = Menu.CreateNode("Setting", pageIndex);
+            Menu.CreateChildNode(parent, "Disk", ++pageIndex);
+            Menu.CreateChildNode(parent, "Errors", ++pageIndex);
+            Menu.CreateChildNode(parent, "Logging", ++pageIndex);
+            pageIndex = 200;
+            parent = Menu.CreateNode("Test", pageIndex);
+            Menu.CreateChildNode(parent, "Test", ++pageIndex);
+            Menu.CreateChildNode(parent, "Testting", ++pageIndex);
+            Menu.CreateChildNode(parent, "Log", ++pageIndex);
+            Menu.CreateChildNode(parent, "Information", ++pageIndex);
+            Disk Page1 = new Disk();
+            uiTabControl1.AddPage(new Disk());
+        }
+        public void Init_Disk_Information()
+        {
+            ManagementClass Diskobject = new ManagementClass("Win32_DiskDrive");//获取一个磁盘实例对象
+            ManagementObjectCollection moc = Diskobject.GetInstances();//获取对象信息的集合            
+            int id = 0;
+            foreach (ManagementObject mo in moc)
+            {
+                if (mo.Properties["InterfaceType"].Value.ToString() == "USB")
+                {
+                    try
+                    {
+                        //产品名称
+                        string name = mo.Properties["Name"].Value.ToString();
+                        string sector_size_s = mo.Properties["TotalSectors"].Value.ToString();
+                        long sector_size = Convert.ToInt64(sector_size_s);
+
+                        string size_s = mo.Properties["Size"].Value.ToString();
+                        double size_d = Convert.ToDouble(size_s)/(1024*1024*1024);
+                        decimal size = decimal.Round(decimal.Parse("" + size_d),2);
+                        //long size = Convert.ToInt64(size_s);
+                        DiskInformation d = new DiskInformation(name, sector_size, size, id++);
+                        Disk_Informaion_List.Add(d);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            if(id==0)
+            {
+                MessageBox.Show("未检测到设备！");
+            }
+        }
+        public void Init_Disk_Driver()
+        {
+            if(Disk_Informaion_List.Count<=0)
+            {
+                MessageBox.Show("未检测到设备！");
+                return;
+            }
+            for(int i=0;i<Disk_Informaion_List.Count;i++)
+            {
+                DiskInformation information = (DiskInformation)Disk_Informaion_List[i];
+                DriverLoader driver = new DriverLoader(information);
+                Disk_Driver_List.Add(driver);
+            }
+        }
+        public void OrderWriteAndVerify(int driver_index,int percent=100,int test_mode=0,int block_size=1,int circle=1)
+        {
+            if(Disk_Driver_List.Count<=0)
+            {
+                MessageBox.Show("未检测到设备！");
+                return;
+            }
+            DriverLoader driver = (DriverLoader)Disk_Driver_List[driver_index];
+            TestArray = new byte[DEAFAUT_BLOCKSIZE * block_size];
+            CompareArray = new byte[DEAFAUT_BLOCKSIZE * block_size];
+            //long actual_size = ((driver.DiskInformation.DiskSectorSize / block_size)*percent)/100;
+            long actual_size = 10;
+            if(test_mode==0||test_mode==1)
+            {
+                int error_num = 0;
+                Init_TestArray(block_size, test_mode);
+                for(long i=0;i<actual_size;i++)
+                {
+                    driver.WritSector(TestArray, i, block_size);
+                    CompareArray = driver.ReadSector(i, block_size);
+                    error_num += VerifyArray(TestArray, CompareArray);
+                }
+                if (error_num == 0)
+                    Console.WriteLine("读写测试完成，测试了"+actual_size+"次未发生错误！");
+            }
+            else if(test_mode==2)
+            {
+                int error_num = 0;
+                for (long i = 0; i < actual_size; i++)
+                {
+                    Init_TestArray(block_size, test_mode);
+                    driver.WritSector(TestArray, i, block_size);
+                    CompareArray = driver.ReadSector(i, block_size);
+                    error_num += VerifyArray(TestArray, CompareArray);
+                }
+                if (error_num == 0)
+                    Console.WriteLine("读写测试完成，测试了" + actual_size + "次未发生错误！");
+            }
+            else
+            {
+                Console.WriteLine("测试模式不存在，请重新选择!");
+            }
+        }
+
+        public void Init_TestArray(int block_size,int mode)
+        {
+            if (block_size == 0)
+            {
+                MessageBox.Show("块大小不能为0");
+                return;
+            }              
+            if(mode==0)
+            {
+                for (int i = 0; i < block_size * DEAFAUT_BLOCKSIZE; i++)
+                {
+                    TestArray[i] = 0;
+                }
+            }
+            else if(mode==1)
+            {
+                for (int i = 0; i < block_size * DEAFAUT_BLOCKSIZE; i++)
+                {
+                    TestArray[i] = 255;
+                }
+            }
+            else if(mode==2)
+            {
+                Random R = new Random();
+                for (int i = 0; i < block_size * DEAFAUT_BLOCKSIZE; i++)
+                {
+
+                    TestArray[i] = (byte)R.Next(0,255);
+                }
+            }
+            
+        }
+        public int VerifyArray(byte[] testarray,byte[] comparearray)
+        {
+            if(testarray.Length!=comparearray.Length)
+            {
+                Console.WriteLine("数组长度不匹配！");
+            }
+            int error_num=0;
+            for(int i = 0; i < testarray.Length; i++)
+            {
+                if(testarray[i]!=comparearray[i])
+                {
+                    Console.WriteLine("当前位置" + i + "出错，正确数据为" + testarray[i] + "错误数据为：" + comparearray[i]);
+                    error_num++;
+                }
+            }
+            return error_num;
+
+        }
+        public long NextLong(long A, long B)
+        {
+            Random R = new Random((int)DateTime.Now.Ticks);
+            long myResult = A;
+            //-----
+            long Max = B, Min = A;
+            if (A > B)
+            {
+                Max = A;
+                Min = B;
+            }
+            double Key = R.NextDouble();
+            myResult = Min + (long)((Max - Min) * Key);
+            //-----
+            return myResult;
+        }
+        private void Menu_MenuItemClick(TreeNode node, Sunny.UI.NavMenuItem item, int pageIndex)
+        {
+            Disk Page1 = new Disk();
+            Errors Page2 = new Errors();
+            Test2 Page4 = new Test2();
+            Logging Page3 = new Logging();
+            Testing Page5 = new Testing();
+            Information Page6 = new Information();
+            Log Page7 = new Log();
+            switch (pageIndex)
+            {
+                case 100:
+                    break;
+                case 101:
+                    uiTabControl1.Controls.Clear();
+                    uiTabControl1.AddPage(Page1);
+                    break;
+                case 102:
+                    uiTabControl1.Controls.Clear();
+                    uiTabControl1.AddPage(Page2);
+                    break;
+                case 103:
+                    uiTabControl1.Controls.Clear();
+                    uiTabControl1.AddPage(Page3);
+                    break;
+                case 201:
+                    uiTabControl1.Controls.Clear();
+                    uiTabControl1.AddPage(Page4);
+                    break;
+                case 202:
+                    uiTabControl1.Controls.Clear();
+                    uiTabControl1.AddPage(Page5);
+                    break;
+                case 203:
+                    uiTabControl1.Controls.Clear();
+                    uiTabControl1.AddPage(Page7);
+                    break;
+                case 204:
+                    uiTabControl1.Controls.Clear();
+                    uiTabControl1.AddPage(Page6);
+                    break;
+
+            }
+        }
+    }
+}
